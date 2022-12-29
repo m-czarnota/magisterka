124d68220e1bcd3b5a02b6c166818a0990aad80a
m-czarnota <michal.czarnota.1998@gmail.com>
2022-12-29 15:32:57 +0100

    configure project files

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// bootstrap
+import 'bootstrap'
+
+// start the Stimulus application
+import './bootstrap';
